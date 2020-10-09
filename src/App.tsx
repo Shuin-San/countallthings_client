ddbe040480faf3b8d72c12b0da436fd3d36a69de
Shuin <shuingr@gmail.com>
2020-10-09 08:53:55 +0200

    ινιτηιαλ ψομμιτ

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {Button} from '@material-ui/core'
 import logo from './logo.svg';
 import './App.css';
+import io from "socket.io-client";
+
+const ENDPOINT = "https://clickerserversgr.herokuapp.com/";
+
 
 function App() {
+
+  const [counter, setCounter] = useState({counter: Number});
+  const socket = io(ENDPOINT);
+  useEffect(() => {
+    socket.emit("getCounter");
+    socket.on("counter", data => {
+      setCounter(data);
+    });
+
+    // CLEAN UP THE EFFECT
+    return () => socket.disconnect();
+    //
+  }, []);
+
+  useEffect(() => {
+    console.log(counter)
+  }, [counter]);
+
+  const incrementCounter = (n) => {
+    socket.emit("updateCounter", 'increment', n);
+  };
+
+  const decrementCounter = (n) => {
+    socket.emit("updateCounter", 'decrement', n);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +41,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {counter.counter}
+        <Button onClick={()=>incrementCounter(1)}>+1</Button>
+        <Button onClick={()=>decrementCounter(1)}>-1</Button>
       </header>
     </div>
   );
